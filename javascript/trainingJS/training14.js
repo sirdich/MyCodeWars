4c0d7f9211ef54a1cbd22baad4f0f13d8f8471a1
sirdich <sirdicholas@gmail.com>
2021-08-25 21:09:53 +0100

    more elegent solution found on codewars

--- javascript/trainingJS/training14.js
+++ javascript/trainingJS/training14.js
@@ -1,21 +1,29 @@
+// function colorOf(r, g, b) {
+//   red = r.toString(16);
+//   if (red.length < 2) {
+//     red = '0' + red;
+//   }
+//   green = g.toString(16);
+//   if (green.length < 2) {
+//     green = '0' + green;
+//   }
+//   blue = b.toString(16);
+//   if (blue.length < 2) {
+//     blue = '0' + blue;
+//   }
+
+//   return '#' + red + green + blue;
+// }
+
 function colorOf(r, g, b) {
-  red = r.toString(16);
-  if (red.length < 2) {
-    red = '0' + red;
-  }
-  green = g.toString(16);
-  if (green.length < 2) {
-    green = '0' + green;
-  }
-  blue = b.toString(16);
-  if (blue.length < 2) {
-    blue = '0' + blue;
-  }
-
-  //   console.log('#' + red + green + blue);
-
-  return '#' + red + green + blue;
+  return '#' + toHex(r) + toHex(g) + toHex(b);
 }
+
+function toHex(n) {
+  var result = n.toString(16);
+  return result.length === 1 ? '0' + result : result;
+}
+
 console.log(colorOf(255, 0, 0) === '#ff0000');
 console.log(colorOf(0, 111, 0) === '#006f00');
 console.log(colorOf(1, 2, 3) === '#010203');
